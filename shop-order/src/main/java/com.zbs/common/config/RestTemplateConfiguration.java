d95b9b774b202c76d459955711111b2d6ffb9df9
zbs <250468591@qq.com>
2023-03-16 20:20:39 +0800

    feat: IRule在这里定义Ribbon负载均衡的规则

--- shop-order/src/main/java/com.zbs/common/config/RestTemplateConfiguration.java
+++ shop-order/src/main/java/com.zbs/common/config/RestTemplateConfiguration.java
@@ -1,5 +1,7 @@
 package com.zbs.common.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +20,15 @@ public class RestTemplateConfiguration {
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * 在这里定义Ribbon负载均衡的规则
+     * @return
+     */
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();
     }
 
 }
