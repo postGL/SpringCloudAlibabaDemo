6f5d2ab69f9bffcbe86708c86515aff9449b95cc
zbs <250468591@qq.com>
2023-03-16 16:57:45 +0800

    feat: 订单服务【随机！】调用多个商品微服务中的一个

--- shop-order/src/main/java/com.zbs/controller/OrderController.java
+++ shop-order/src/main/java/com.zbs/controller/OrderController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * description: OrderController
@@ -37,6 +38,8 @@ public class OrderController {
 
     /**
      * 下单
+     * 第三次：自定义客户端随机选择服务。
+     * 商品服务是集群，通过客户端选择调用哪个服务。
      * @param pid
      * @return
      */
@@ -45,7 +48,10 @@ public class OrderController {
         log.info("接收到{}号商品的下单请求，接下来调用商品微服务查询此商品信息", pid);
         // 调用商品微服务
         List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
-        ServiceInstance serviceInstance = instances.get(0);
+        // 随机选择，获取随机数
+        int index = new Random().nextInt(instances.size());
+
+        ServiceInstance serviceInstance = instances.get(index);
 
         Product product = restTemplate.getForObject("http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/product/" + pid, Product.class);
         log.info("查询到{}号商品信息是：{}", pid, JSON.toJSONString(product));
@@ -63,7 +69,34 @@ public class OrderController {
     }
 
 //    /**
-//     * 下单
+//     * 下单 第二次：通过Nacos注册中心获取商品微服务实例，获取实例信息（IP和端口）
+//     * @param pid
+//     * @return
+//     */
+//    @RequestMapping("order/{pid}")
+//    public Order order(@PathVariable("pid") Integer pid) {
+//        log.info("接收到{}号商品的下单请求，接下来调用商品微服务查询此商品信息", pid);
+//        // 调用商品微服务
+//        List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
+//        ServiceInstance serviceInstance = instances.get(0);
+//
+//        Product product = restTemplate.getForObject("http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/product/" + pid, Product.class);
+//        log.info("查询到{}号商品信息是：{}", pid, JSON.toJSONString(product));
+//        // 下单
+//        Order order = new Order();
+//        order.setUid(1);
+//        order.setUsername("测试用户");
+//        order.setPid(product.getPid());
+//        order.setPname(product.getPname());
+//        order.setPprice(product.getPprice());
+//        order.setNumber(1);
+//        Order order1 = orderService.createOrder(order);
+//        log.info("创建一个订单成功，订单是：{}", JSON.toJSONString(order1));
+//        return order1;
+//    }
+
+//    /**
+//     * 下单 第一次：通过注入restTemplate，写死固定调用URL获取
 //     * @param pid
 //     * @return
 //     */
