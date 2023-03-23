43bcc97cd194ebb5fdd7e17232e6400507f5193e
zbs <250468591@qq.com>
2023-03-23 20:18:33 +0800

    feat: Sentinel @SentinelResource自定义-处理异常

--- shop-order/src/main/java/com.zbs/controller/OrderSentinelController2.java
+++ shop-order/src/main/java/com.zbs/controller/OrderSentinelController2.java
@@ -1,6 +1,7 @@
 package com.zbs.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,10 +31,41 @@ public class OrderSentinelController2 {
         return "测试Sentinel TWO";
     }
 
+    /**
+     * 定义一个资源
+     * SentinelResource
+     * 定义当资源内部发生异常的时候的处理逻辑
+     * blockHandler 定义当资源内部发生BlockException应该进入方法{捕获的是Sentinel定义的异常}
+     * fallback 定义当资源内部发生了Throwable应该进入的方法
+     */
     @RequestMapping("test2/message3")
-    @SentinelResource("message3")
+    @SentinelResource(value = "message3", blockHandler = "blockHandler", fallback = "fallback")
     public String message2(String name, Integer age) {
         return name + age;
+    }
+
+    /**
+     * blockHandler
+     * 要求：
+     * 1、当前方法的返回值和参数要与原方法（加@SentinelResource注解的方法）一致
+     * 2、但是允许在参数列表的最后加入一个参数BlockException，用来接收原方法中发生的异常
+     */
+    public String blockHandler(String name, Integer age, BlockException e) {
+        // 自定义异常逻辑处理
+        log.error("触发了BlockException，内容为：{}", e);
+        return "BlockException!";
+    }
+
+    /**
+     * fallback
+     * 要求：
+     * 1、当前方法的返回值和参数要与原方法（加@SentinelResource注解的方法）一致
+     * 2、但是允许在参数列表的最后加入一个参数BlockException，用来接收原方法中发生的异常
+     */
+    public String fallback(String name, Integer age, Throwable e) {
+        // 自定义异常逻辑处理
+        log.error("触发了Throwable，内容为：{}", e);
+        return "Throwable!";
     }
 
 }
