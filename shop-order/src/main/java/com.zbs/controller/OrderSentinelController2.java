5cb0c3bc614a228558f359f36187493ed0054b86
zbs <250468591@qq.com>
2023-03-23 15:19:30 +0800

    feat: Sentinel--热点规则

--- shop-order/src/main/java/com.zbs/controller/OrderSentinelController2.java
+++ shop-order/src/main/java/com.zbs/controller/OrderSentinelController2.java
@@ -1,5 +1,6 @@
 package com.zbs.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,23 @@ public class OrderSentinelController2 {
 
     @RequestMapping("test2/message1")
     public String message1() {
-        return "测试高并发";
+//        try {
+//            Thread.sleep(2000L);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return "测试Sentinel ONE";
     }
 
     @RequestMapping("test2/message2")
     public String message2() {
-        return "测试高并发";
+        return "测试Sentinel TWO";
+    }
+
+    @RequestMapping("test2/message3")
+    @SentinelResource("message3")
+    public String message2(String name, Integer age) {
+        return name + age;
     }
 
 }
