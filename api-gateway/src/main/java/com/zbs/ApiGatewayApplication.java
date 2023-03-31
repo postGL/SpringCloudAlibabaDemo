e07dea82143b200555b9952d98fa452e39a8c6f3
zbs <250468591@qq.com>
2023-03-31 09:50:58 +0800

    feat: 通过GateWay网关调用服务

---
+++ api-gateway/src/main/java/com/zbs/ApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.zbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description: ApiGatewayApplication
+ * date: 2023/3/30 20:48
+ * author: zhangbs
+ * version: 1.0
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class);
+    }
+
+}
