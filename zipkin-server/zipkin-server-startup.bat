4d34e53baa00a477ecfcc6f725365ea70bc47cd8
zbs <250468591@qq.com>
2023-04-07 16:19:32 +0800

    feat: zipkin持久化，创建zipkin-server数据库，保存持久化数据，更改启动脚本命令。

--- zipkin-server/zipkin-server-startup.bat
+++ zipkin-server/zipkin-server-startup.bat
@@ -1,1 +1,1 @@
-java -jar zipkin-server-2.12.9-exec.jar
+java -jar zipkin-server-2.12.9-exec.jar --STORAGE_TYPE=mysql --MYSQL_HOST=127.0.0.1 --MYSQL_TCP_PORT=3306 --MYSQL_DB=zipkin-server --MYSQL_USER=root --MYSQL_PASS=root
