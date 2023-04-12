609ce7ce1e6abeaa4fdcc9aec9d04940e6e2207f
zbs <250468591@qq.com>
2023-04-12 10:44:58 +0800

    feat: MQ消息消费，发送短信测试

--- shop-user/src/main/java/com/zbs/mq/SmsService.java
+++ shop-user/src/main/java/com/zbs/mq/SmsService.java
@@ -1,13 +1,22 @@
 package com.zbs.mq;
 
 import com.alibaba.fastjson.JSON;
+import com.zbs.dao.UserDao;
 import com.zbs.domain.Order;
+import com.zbs.domain.User;
+import com.zbs.util.SmsUtil;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.ConsumeMode;
 import org.apache.rocketmq.spring.annotation.MessageModel;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 /**
  * description: SmsService
@@ -29,9 +38,41 @@ import org.springframework.stereotype.Service;
         consumeMode = ConsumeMode.CONCURRENTLY,
         messageModel = MessageModel.CLUSTERING)
 public class SmsService implements RocketMQListener<Order> {
-    // 消费逻辑
+
+    @Autowired
+    private UserDao userDao;
+
+    /**
+     * 消息消费逻辑
+     */
     @Override
     public void onMessage(Order order) {
         log.info("收到一个订单信息{},接下来发送短信", JSON.toJSONString(order));
+        // 发送短信
+        //根据uid 获取手机号
+        User user = userDao.findById(order.getUid()).get();
+
+        //生成验证码
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            builder.append(new Random().nextInt(9) + 1);
+        }
+        String smsCode = builder.toString();
+        Param param = new Param(smsCode);
+        try {
+            //发送短信 {"code":"123456"}
+            SmsUtil.sendSms(user.getTelephone(), "SpriCloudAli", "SMS_276195466", JSON.toJSONString(param));
+            log.info("短信发送成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    class Param {
+        private String code;
+    }
+
 }
